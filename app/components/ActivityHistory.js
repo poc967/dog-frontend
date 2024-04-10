0c1fed4a9b28e8183e17a537cdaa32856eb54e19
Patrick O'Connor <poc.967@gmail.com>
2024-04-09 23:02:46 -0400

    complete v1 of activity history

--- app/components/ActivityHistory.js
+++ app/components/ActivityHistory.js
@@ -1,5 +1,31 @@
-const ActivityHistory = () => {
-  return <span>Activity History</span>;
+import styled from 'styled-components';
+
+const Table = styled.table`
+  width: 100%;
+`;
+
+const ActivityHistory = (props) => {
+  let activityHistory = props.dog.activity_history;
+  return (
+    <Table className="bp5-html-table">
+      <thead>
+        <tr>
+          <th>Time</th>
+          <th>Activity</th>
+          <th>Location</th>
+        </tr>
+      </thead>
+      <tbody>
+        {activityHistory.map((activity, index) => (
+          <tr key={index}>
+            <td>{activity.time}</td>
+            <td>{activity.type}</td>
+            <td>{activity.location}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
 };
 
 export default ActivityHistory;
